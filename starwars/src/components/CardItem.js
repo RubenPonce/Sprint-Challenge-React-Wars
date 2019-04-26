be079be973226555801839e4fc55a79e21c0a6cd
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-04-26 12:22:11 -0700

    mis-typed height instead of mass

--- starwars/src/components/CardItem.js
+++ starwars/src/components/CardItem.js
@@ -8,7 +8,7 @@ const CardItem = (props)=>{
           <li className="character-name">{character.name.toUpperCase()}</li>
           <h3>birth year: {character.birth_year}</h3>
           <h3>height: {character.height}cm</h3>
-          <h3>mass: {character.height}kg</h3>
+          <h3>mass: {character.mass}kg</h3>
           <h3>hair color: {character.hair_color}</h3>
           <h3>skin color: {character.skin_color}</h3>
         </div>
